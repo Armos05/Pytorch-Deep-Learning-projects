8fe9d931edfc82e2e80d93d99ab5781ea1bd31d3
Akhil Ratan Mishra <Akhilratanm@gmail.com>
2023-03-05 14:10:43 +0100

    Delete CIFAR-pytorch-main/CIFAR-pytorch-main directory

--- CIFAR-pytorch-main/CIFAR-pytorch-main/src/loss.py
+++
@@ -1,4 +0,0 @@
-import torch.optim as optim
-
-criterion = nn.CrossEntropyLoss()                    #Defining the Loss function
-optimizer = optim.SGD(net.parameters(), lr = 0.001, momentum = 0.8)       #Stochastic Gradient Descent Optimization
